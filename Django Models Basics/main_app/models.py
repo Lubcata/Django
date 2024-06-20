df24c635b22c6458066d3e03cc1144fd8bb714bc
Luboslav Florov <144922507+Lubcata@users.noreply.github.com>
2024-06-20 22:29:58 +0300

    Add Task

---
+++ Django Models Basics/main_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    email_address = models.EmailField()
+    photo = models.URLField()
+    birth_date = models.DateField()
+    works_full_time = models.BooleanField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+class Department(models.Model):
+    code = models.CharField(max_length=4, primary_key=True, unique=True)
+    name = models.CharField(max_length=50, unique=True)
+    employees_count = models.PositiveIntegerField(default=1)
+
+
+        
