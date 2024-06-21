2d5798970359249b3cb6fd0564b01cc45ae9fd67
Luboslav Florov <144922507+Lubcata@users.noreply.github.com>
2024-06-21 10:06:04 +0300

    Update Task

--- Django Models Basics Exercise/main_app/admin.py
+++ Django Models Basics Exercise/main_app/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from main_app.models import Book
+from main_app.models import Book, Exercise
 
 
 # Register your models here.
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    pass
+
