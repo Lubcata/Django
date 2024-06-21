f93b6a27d5021c00386d668a3e9a7585787d4e48
Luboslav Florov <144922507+Lubcata@users.noreply.github.com>
2024-06-21 10:49:13 +0300

    Add Task

--- Django Models Basics/main_app/models.py
+++ Django Models Basics/main_app/models.py
@@ -1,18 +1,41 @@
+from datetime import date
+
 from django.db import models
 
 # Create your models here.
+
+
 class Employee(models.Model):
     name = models.CharField(max_length=30)
     email_address = models.EmailField()
     photo = models.URLField()
     birth_date = models.DateField()
     works_full_time = models.BooleanField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateField(auto_now_add=True)
+
+
+class DepartmentChoices(models.TextChoices):
+    SOFIA = "Sf", "Sofia"
+    PLOVDIV = "Pv", "Plovdiv"
+    BURGAS = "Br", "Burgas"
+    VARNA = "Vr", "Varna"
+
 
 class Department(models.Model):
     code = models.CharField(max_length=4, primary_key=True, unique=True)
     name = models.CharField(max_length=50, unique=True)
     employees_count = models.PositiveIntegerField(default=1)
+    location = models.CharField(max_length=20, choices=DepartmentChoices.choices)
+    last_edited_on = models.DateTimeField(auto_now=True, editable=False)
 
 
-        
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+    duration_in_days = models.PositiveIntegerField(verbose_name="Duration in Days")
+    estimated_hours = models.FloatField(verbose_name="Estimated Hours")
+    start_date = models.DateField(default=date.today, verbose_name="Start Date")
+    created_on = models.DateField(auto_now_add=True, editable=False)
+    last_edited_on = models.DateField(auto_now=True, editable=False)
+
--- Django Models Basics/orm_skeleton/settings.py
+++ Django Models Basics/orm_skeleton/settings.py
@@ -75,17 +75,11 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
+        "NAME": "Django Models",
         "USER": "postgres",
         "PASSWORD": "Lubcata7",
         "HOST": "localhost",
