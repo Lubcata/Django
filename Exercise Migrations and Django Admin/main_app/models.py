8f8b6d485e1238c8eaacdcafe6b665d527ca2ef4
Luboslav Florov <144922507+Lubcata@users.noreply.github.com>
2024-06-26 20:04:05 +0300

    edit task

--- Exercise Migrations and Django Admin/main_app/models.py
+++ Exercise Migrations and Django Admin/main_app/models.py
@@ -83,3 +83,6 @@ class Person(models.Model):
 class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    rarity = models.CharField(max_length=20, default='No rarity')
+    
