e43083236776b16b75b9881b0028fb78c0fc3504
Luboslav Florov <144922507+Lubcata@users.noreply.github.com>
2024-07-25 21:25:04 +0300

    Add task

---
+++ Exercise Advanced Queries in Django/caller.py
@@ -0,0 +1,17 @@
+import os
+from datetime import date
+
+import django
+
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, Technology, Project, Programmer, Task, \
+    Exercise
+
+# Run and print your queries
+
+
